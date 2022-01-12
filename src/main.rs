111811bbc38ab6924f8a0ee9b13c86e75939e436
Florian7843 <floriansch7843@gmail.com>
2022-01-12 14:53:48 +0100

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use adw::Window;
+use gtk::prelude::*;
+use gtk::Application;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new application
+    let app = Application::builder()
+        .application_id("org.gtk-rs.example")
+        .build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Init `gtk::Builder` from file
+    let builder = gtk::Builder::from_string(include_str!("ui/window.ui"));
+
+    // Get window and button from `gtk::Builder`
+    let window: Window = builder
+        .object("window")
+        .expect("Could not get object `window` from builder.");
+
+    // Set application
+    window.set_application(Some(app));
+
+    window.present();
 }
